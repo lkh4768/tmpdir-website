457b7947524327ae20d8e5f9a550836e518ab097
wes.lee <lkh5510@gmail.com>
2017-09-24 18:02:40 +0900

    test jenkins groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,11 +3,11 @@ node {
 		developBranchName = "develop"
 		releaseBranchName = "master"
 		gradle = tool 'gradle'
-		echo 'env.BRANCH_NAME BRANCH_NAME developBranchName'
+		echo env.BRANCH_NAME BRANCH_NAME developBranchName
 	}
 
 	stage("Checkout") {
-		echo 'env.BRANCH_NAME BRANCH_NAME developBranchName'
+		echo env.BRANCH_NAME BRANCH_NAME developBranchName
 		if(env.BRANCH_NAME == developBranchName){
 			checkout scm
 		}
