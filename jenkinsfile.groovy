d7e01d0c728295b95d5635903e4a144a65c4ce93
wes.lee <lkh5510@gmail.com>
2017-09-25 12:53:25 +0900

    test jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -49,7 +49,7 @@ node {
 	
 	stage("Deploy on stage") {
 		if(env.BRANCH_NAME == developBranchName){
-			image = docker.build("${imageName}", "--build-arg PACKAGE_NAME=${packageName} PACKAGE_VERSION=${packageVersion}")
+			image = docker.build("${imageName}", "--build-arg PACKAGE_NAME=${packageName}", "--build-arg PACKAGE_VERSION=${packageVersion}")
 			sh "docker rm -f ${containerName}"
 			image.withRun("-p 80:80 --name ${containerName}")
 		}
