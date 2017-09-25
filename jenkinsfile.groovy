3dfa4499affe24db65b270efef38a213223d1569
wes.lee <lkh5510@gmail.com>
2017-09-25 11:47:06 +0900

    test jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,10 +5,8 @@ node {
 		gradle = tool 'gradle'
 		packageName = sh script: "${gradle}/bin/gradle properties | grep name | awk '{print \$2}' | tr -d '\\n'", returnStdout: true
 		packageVersion = sh script: "${gradle}/bin/gradle properties | grep version | awk '{print \$2}' | tr -d '\\n'", returnStdout: true
-		imageName = "tmpdir-${packageName}:${packageVersion}"
-		imageName.toLowerCase()
-		containerName = "tmpdir-${packageName}-${packageVersion}"
-		containerName.toLowerCase()
+		imageName = "tmpdir-${packageName[0].toLowerCase()}:${packageVersion}"
+		containerName = "tmpdir-${packageName[0].toLowerCase()}-${packageVersion}"
 		echo "${packageName}, ${packageVersion}, ${imageName}, ${containerName}"
 	}
 
