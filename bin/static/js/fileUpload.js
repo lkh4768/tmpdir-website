257c6958af797ab9d451f5f5967eb5541c249820
wes.lee <lkh5510@gmail.com>
2017-10-19 00:17:34 +0900

    resolving #24

--- bin/static/js/fileUpload.js
+++ bin/static/js/fileUpload.js
@@ -1,28 +1,6 @@
 /**
  * Created by wes.lee on 2017-09-29.
  */
-
-/* facebook share api */
-(function (d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.10";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-/* twitter share api */
-!function (d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
-  if (!d.getElementById(id)) {
-    js = d.createElement(s);
-    js.id = id;
-    js.src = p + '://platform.twitter.com/widgets.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }
-}(document, 'script', 'twitter-wjs');
-
 (function () {
   new Clipboard('#copy-btn');
 })();
@@ -54,7 +32,7 @@ $(document).ready(
         var expireTime = new Date(response.expireTime);
         $(".overlay").css("display", "table");
         $(".upload").css("display", "none");
-        $("#download-url").val("http://tmpdir.sw-warehouse.xyz/download/" + response.id);
+        $("#download-url").val("http://tmpdir.sw-warehouse.xyz/" + response.id);
         $("#expire-time").text(expireTime.toLocaleString([], {
             year: 'numeric',
             month: '2-digit',
