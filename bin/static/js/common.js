495461389dd26c38c450b2c46790d5d7b87a30dc
wes.lee <lkh5510@gmail.com>
2017-11-01 23:20:02 +0900

    add viewport

--- bin/static/js/common.js
+++ bin/static/js/common.js
@@ -3,12 +3,11 @@
  */
 
 /* facebook share api */
-(function (d, s, id) {
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.10";
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.10';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
