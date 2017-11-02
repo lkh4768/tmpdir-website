c2d5e0c8d06d2374559c5291f09cffc195415f3f
wes.lee <lkh5510@gmail.com>
2017-11-02 18:00:36 +0900

    add locale

--- src/main/java/xyz/swwarehouse/tmpdir/MvcConfig.java
+++ src/main/java/xyz/swwarehouse/tmpdir/MvcConfig.java
@@ -1,7 +1,5 @@
 package xyz.swwarehouse.tmpdir;
 
-import java.util.Locale;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
