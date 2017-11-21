c182816abbe01d45d2b4d3c4bf7968e48ccb7e42
wes.lee <lkh5510@gmail.com>
2017-11-21 12:20:23 +0900

    resolved sonarqube issues

--- src/main/java/xyz/swwarehouse/tmpdir/util/Util.java
+++ src/main/java/xyz/swwarehouse/tmpdir/util/Util.java
@@ -3,18 +3,18 @@ package xyz.swwarehouse.tmpdir.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class Util {
-	static public String GetClientInfo(final HttpServletRequest req) {
+	static public String getClientInfo(final HttpServletRequest req) {
 		String addr = req.getHeader("X-FORWARDED-FOR");
 		if (addr == null)
 			addr = req.getRemoteAddr();
 		return addr + ":" + req.getRemotePort();
 	}
 	
-	static public String GetLocalInfo(final HttpServletRequest req) {
+	static public String getLocalInfo(final HttpServletRequest req) {
 		return req.getLocalAddr() + ":" + req.getLocalPort();
 	}
 	
-	static public String RequestInfoToString(final HttpServletRequest req) {
-		return "Request " + req.getMethod() + " (" + GetClientInfo(req) + "->" + GetLocalInfo(req) + "), " + req.getRequestURL();
+	static public String requestInfoToString(final HttpServletRequest req) {
+		return "Request " + req.getMethod() + " (" + getClientInfo(req) + "->" + getLocalInfo(req) + "), " + req.getRequestURL();
 	}
 }
