8e09263abf7717841d17bcc39ea3898c12403eed
wes.lee <lkh5510@gmail.com>
2018-01-27 14:29:39 +0900

    resolved #13

--- src/main/java/xyz/swwarehouse/tmpdir/controller/WebsiteController.java
+++ src/main/java/xyz/swwarehouse/tmpdir/controller/WebsiteController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
