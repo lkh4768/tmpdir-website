c73155b7a6a8dc5ce0c263ca8d0988e2fdb95d36
wes.lee <lkh5510@gmail.com>
2018-01-30 00:59:45 +0900

    resolving #41 - update webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,3 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true
-	},
 	"extends": "airbnb"
-	"rules": {
-		"indent": [
-			"error",
-			2
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"single"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	},
-	"parserOptions": {
-		"sourceType": "module"
-	}
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 module.exports = {
-	entry: './src/index.js',
+	context: __dirname + '/src',
+	entry: {
+		server: './server.js'
+	},
 	output: {
-		path: __dirname,
-		filename: 'bundle.js'
+		path: __dirname + '/build',
+		filename: '[name].bundle.js'
 	},
 	module: {
-		loaders: [
-			{ test: /\.sass$/, loader: 'style!css!sass' }
+		rules:[
+			{
+				test: /\.js$/,
+				enforce: 'pre',
+				loader: 'eslint-loader',
+				exclude: /(node_modules|bower_components)/
+			},
+			{ 
+				test: /\.sass$/, 
+				loader: 'style!css!sass'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /(node_modules|bower_components)/,
+				query: {
+					cacheDirectory: true,
+					presets: ['es2015', 'react']
+				}
+			}
 		]
 	}
 };
