1369b6b56c608c16f1050caf74046f29a7710e80
wes.lee <lkh5510@gmail.com>
2018-02-04 23:59:58 +0900

    resolving #41 - update webpack, ...

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,30 +3,40 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval-source-map',
-	entry: [
-		path.join(__dirname, '/src/public/index.js')
-	],
-	output: {
-		path: path.join(__dirname + '/build/'),
-		filename: '[name].js',
-		publicPath: '/'
-	},
-	plugins: [
+  entry: [
+    path.join(__dirname, '/src/public/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/build/'),
+    filename: '[name]-[hash].min.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/public/pages/index.ejs',
       filename: 'index.html'
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('[name]-[hash].min.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
-	module: {
+  module: {
 		rules:[
 			{
 				test: /\.ejs$/,
@@ -58,5 +68,5 @@ module.exports = {
 				}
 			},
 		]
-	}
+  }
 };
