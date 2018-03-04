779a0d2a2e0636b939f1cca0c4b931a863628dfb
lkh4768 <lkh5510@gmail.com>
2018-03-04 13:59:16 +0900

    resolved #12

--- src/public/containers/Body/BodyRow.jsx
+++ src/public/containers/Body/BodyRow.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 import { Row, Col } from 'reactstrap';
 
 class BodyRow extends React.Component {
+  getClassName() {
+    return [this.props.className, this.props.align].join(' ');
+  }
+
   render() {
-    const className = 'body__row__col';
+    const className = this.getClassName();
     const ele = (
       <Row>
         <Col className={className}>
@@ -17,8 +21,21 @@ class BodyRow extends React.Component {
   }
 }
 
+BodyRow.ALIGN = {
+  left: 'body__row__col',
+  right: 'body__row__col--right',
+  center: 'body__row__col--center',
+};
+
 BodyRow.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  align: PropTypes.string,
+};
+
+BodyRow.defaultProps = {
+  className: '',
+  align: BodyRow.ALIGN.left,
 };
 
 export default BodyRow;
