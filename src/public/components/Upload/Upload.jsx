77e8ad43d1be1b6c44a452f308ac11a996557594
lkh4768 <lkh5510@gmail.com>
2018-04-11 00:43:45 +0900

    resolving #5

--- src/public/components/Upload/Upload.jsx
+++ src/public/components/Upload/Upload.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Row, Col } from 'reactstrap';
+import { Button } from 'reactstrap';
+import BodyRow from '../Body/BodyRow';
 
 class Upload extends React.Component {
   render() {
     const node = (
-      <Row>
-        <Col />
-      </Row>
+      <BodyRow align={BodyRow.ALIGN.between}>
+        <span>최대 용량: 1GB, 보관기간: 1일</span>
+        <Button color="primary" size="lg">Upload</Button>
+      </BodyRow>
     );
 
     return node;
