33c6c78ba8ca9e2c0fe27f1ac0ba1d8f65337c0f
lkh4768 <lkh5510@gmail.com>
2018-04-12 23:42:58 +0900

    resolving #5

--- src/public/components/Upload/Upload.jsx
+++ src/public/components/Upload/Upload.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import BodyRow from '../Body/BodyRow';
+import { post } from 'axios';
 
 class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.uploadFiles = this.uploadFiles.bind(this);
+  }
+  uploadFiles() {
+    const url = 'files';
+    const formData = new FormData();
+    formData.append('file', this.props.files)
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    post(url, formData,config).then((response) => {
+      console.log(response);
+      return null;
+    });
+  }
   render() {
     const node = (
       <BodyRow align={BodyRow.ALIGN.between}>
         <span>최대 용량: 1GB, 보관기간: 1일</span>
-        <Button color="primary" size="lg">Upload</Button>
+        <Button color="primary" size="lg" onClick={this.uploadFiles}>Upload</Button>
       </BodyRow>
     );
 
