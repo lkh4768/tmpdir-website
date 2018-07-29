ef5dc0351250c5587e49a44bf619c8ef132baf46
lkh4768 <lkh5510@gmail.com>
2018-07-29 17:31:28 +0900

    [UPDATE] unit test of render
    Update unit test of render - part of render(getStore, getApp) move const
    Update const - import component, store in appType
    
    Resolving #40

--- src/server/modules/render/index.js
+++ src/server/modules/render/index.js
@@ -1,13 +1,7 @@
-import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import { IntlProvider } from 'react-intl';
 
-import uploadReducer from '_data/reducers/upload';
-import downloadReducer from '_data/reducers/download';
-import UploadApp from '_app/Upload/App';
-import DownloadApp from '_app/Download/App';
 import Utils from '_modules/common/utils';
 import Const from '_modules/common/const';
 import manifest from '_modules/manifest';
@@ -16,36 +10,11 @@ import getConfig from '_modules/config';
 
 const Config = getConfig();
 
-const getStore = (id) => {
-  switch (id) {
-    case Const.appType.upload.id:
-      return createStore(uploadReducer);
-    default:
-      return createStore(downloadReducer);
-  }
-};
-
-const getApp = (id, store, lang) => {
-  let app;
-  switch (id) {
-    case Const.appType.upload.id:
-      app = <UploadApp />;
-      break;
-    default:
-      app = <DownloadApp />;
-      break;
-  }
-  return (
-    <Provider store={store}>
-      <IntlProvider locale={lang}>
-        {app}
-      </IntlProvider>
-    </Provider>
-  );
-};
-
 const render = (type, lang) => {
-  const store = getStore(type.id);
+  if (!type) {
+    logger.error({ type, lang }, 'render type is empty');
+    return '';
+  }
   const html = `
     <!doctype html>
     <html>
@@ -54,18 +23,18 @@ const render = (type, lang) => {
       </head>
       <body>
         <div id="root">
-          ${renderToString(getApp(type.id, store, lang))}
+          ${renderToString(<Provider store={type.store}><IntlProvider locale={lang}>{type.component}</IntlProvider></Provider>)}
         </div>
-      <script>
-        ${Const.windowInitialVar.state} = ${Utils.stringifyState(store.getState())}
-        ${Const.windowInitialVar.lang} = "${lang}"
-      </script>
+        <script>
+          ${Const.windowInitialVar.state} = ${Utils.stringifyState(type.store.getState())}
+          ${Const.windowInitialVar.lang} = "${lang}"
+        </script>
         ${Utils.makeJsNode(manifest.getJsUri(Const.vendor))}
         ${Utils.makeJsNode(manifest.getJsUri(type.name))}
       </body>
     </html>
   `;
-  logger.info({ type, lang, html }, 'render html of type');
+  logger.info({ type, lang, html }, 'render html info');
   return html;
 };
 
