9a96f02f0f851cafe16eada6ab6d254d7ebc5fdc
lkh4768 <lkh5510@gmail.com>
2018-11-05 00:46:39 +0900

    Revese page route

--- src/server/routes/page/index.js
+++ src/server/routes/page/index.js
@@ -1,6 +1,19 @@
-module.exports = () => {
-  return {
-    '/': { page: '/account' },
-    '/dashboard': { page: '/dashboard' },
-  },
-};
+import express from 'express';
+
+import render from '_modules/render';
+import Const from '_modules/common/const';
+import Utils from '_modules/common/utils';
+
+const router = express.Router();
+
+router.get('/', (req, res) => res.end(render(
+  Const.appType.upload,
+  Utils.getLangInAcceptLangHeader(req.headers['accept-language']),
+)));
+
+router.get('/:id', (req, res) => res.end(render(
+  Const.appType.download,
+  Utils.getLangInAcceptLangHeader(req.headers['accept-language']),
+)));
+
+export default router;
